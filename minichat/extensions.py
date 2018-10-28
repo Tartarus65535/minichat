1bfbf93ac8fea5309a73927e69bfe7413d3ed7ad
Tarus <893912498@qq.com>
2018-10-28 22:28:20 +0800

    first

---
+++ minichat/extensions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from flask_login import LoginManager
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+csrf = CSRFProtect()
+moment = Moment()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from minichat.models import User
+    return User.query.get(int(user_id))
+
+
+login_manager.login_view = 'auth.login'
